cd2bb63bfcb0667ab576a76d76898fe4bd82c249
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2022-12-27 22:03:45 +0000

    added eslint

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,18 @@
-require("dotenv").config();
-const express = require("express");
-const helmet = require("helmet");
+require('dotenv').config();
+const express = require('express');
+
+const helmet = require('helmet');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
 
-app.get("/health_check", (req, res) => {
-    return res.json({ status: "application running" });
+app.get('/health_check', (req, res) => {
+  res.json({ status: 'application running' });
 });
 
 app.listen(PORT, async () => {
-    console.log(`Server connnected on port ${PORT}`);
+  console.log(`Server connnected on port ${PORT}`);
 });
