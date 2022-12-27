0a79d37fa4c7e485c6a2c0a796c20aa6ba7067c7
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2022-12-27 15:03:09 +0000

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const helmet = require("helmet");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+
+app.get("/health_check", (req, res) => {
+    return res.json({ status: "application running" });
+});
+
+app.listen(PORT, async () => {
+    console.log(`Server connnected on port ${PORT}`);
+});
